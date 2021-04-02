3f5ff5306338f7bf229c8d1436bed23df06ec4e0
Holy-Feast <shelofasstov@gmail.com>
2021-04-03 00:06:46 +0300

    added full project, 1 bug left

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
+const express = require('express')
 
+const mongoose = require('mongoose')
+
+const exhbs = require('express-handlebars')
+
+const todoRoutes = require('./routes/todos')
+
+const PORT = process.env.PORT || 3000
+
+const app = express()
+const hbs = exhbs.create({
+  defaultLayout: 'main',
+  extname: 'hbs'
+})
+
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+app.set('views', 'views')
+
+app.use(express.urlencoded({ extended: true}))
+
+app.use(todoRoutes)
+
+async function start(){
+  try{
+    await mongoose.connect('mongodb+srv://alexsh:5336shellydb@cluster0.s5ru8.mongodb.net/todos', {
+      useNewUrlParser: true,
+      useFindAndModify: false
+    })
+
+
+    app.listen(PORT, () => {
+      console.log('server has been started...')
+    })
+  }catch(e){
+    console.log(e)
+  }
+}
+
+start()
